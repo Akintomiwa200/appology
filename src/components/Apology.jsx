c78d0ac8ca6eba82cedc0bfede071d682efb81a6
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-07-20 00:50:25 +0100

    Update Apology.jsx

--- src/components/Apology.jsx
+++ src/components/Apology.jsx
@@ -3,8 +3,8 @@ export default function Apology() {
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="flex flex-col justify-center items-center">
-        <h1 className="text-blue-700 w-[39vw] py-2 border border-blue-700 text-xl lg:text-3xl">Apology</h1>
-        <p className="w-[60vw] my-8 p-4 border border-blue-700 text-xs lg:text-2xl text-justify">
+        <h1 className="text-blue-700 flex text-center rounded items-center justify-center w-[39vw] py-2 border border-blue-700 text-xl lg:text-3xl">Apology</h1>
+        <p className="w-[60vw] rounded my-8 p-4 border border-blue-700 text-xs lg:text-2xl text-justify">
           <ol>
             <li className="text-xs lg:text-2xl text-justify">I am very sorry for the disorganisation</li>
             <li className="text-xs lg:text-2xl text-justify">I am sorry for allowing impatience to take a toll on very affected party</li>
