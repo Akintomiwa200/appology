a07b4c9247da701b1c286378309f1758d354231f
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-07-20 00:51:19 +0100

    Update Offence.jsx

--- src/components/Offence.jsx
+++ src/components/Offence.jsx
@@ -3,8 +3,8 @@ export default function Offence() {
     return (
         <div className="bg-white py-24 sm:py-32">
             <div className="flex flex-col justify-center items-center">
-                <h1 className="text-blue-700 w-[39vw] py-2 border border-blue-700 text-xl lg:text-3xl">My offence</h1>
-                <p className="w-[60vw] my-8 p-4 border border-blue-700 text-xs lg:text-2xl text-justify">i offended by removing someone that has been assigned to a task and assigning the task to myself.
+                <h1 className="text-blue-700 w-[39vw] py-2 border border-blue-700 rounded text-center text-xl lg:text-3xl">My offence</h1>
+                <p className="w-[60vw] my-8 p-4 rounded border border-blue-700 text-xs lg:text-2xl text-justify">i offended by removing someone that has been assigned to a task and assigning the task to myself.
                     which in turn has affected the person by slowing down her workrate and causing someone else to asign the task to himself
                 </p>
             </div>
